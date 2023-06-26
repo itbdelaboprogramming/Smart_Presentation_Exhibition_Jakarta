19521cb083d895155fb7efba644cdfc4ce5e1c39
adeliaaaa <monicadelia88@gmail.com>
2023-06-26 11:15:33 +0700

    feat: initial commit (3D obj rendered)

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    $global_variable = "MSD700_bucket_MCLA007A_00_2.glb";
+    // if (isset($_GET['value'])) {
+    //     $value = urldecode($_GET['value']);
+    //     $global_variable = $value;
+    // }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Smart Presentation</title>
+        <!-- <link rel="icon" type="image/x-icon" href="assets/logo.png"> -->
+        <meta charset="UTF-8">
+        <script type="importmap">
+            {
+                "imports": {
+                "three": "https://unpkg.com/three@0.153.0/build/three.module.js"
+                }
+            }
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/gsap@3.2.4/dist/gsap.js"></script>
+        <link rel="stylesheet" href="./style/style.css" >
+    </head>
+    <body>
+        <div class="home-page">
+            <canvas id="myCanvas">    </canvas>
+            <script type="module" src="script.js"> </script>
+        </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,111 @@
+const myCanvas = document.querySelector("#myCanvas");
+// var myText = document.getElementById("myText").textContent;
+
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://unpkg.com/three@0.139.2/examples/jsm/loaders/GLTFLoader.js";
+
+// Creating a scene with background color
+export const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x1d2538);
+// 0xdbe9e9 = light blue
+// 0xe0e4e7 = light gray
+
+// Plane geometry as a ground
+const geometry = new THREE.PlaneGeometry(20, 20, 8, 8);
+const material = new THREE.MeshBasicMaterial({
+	color: 0x4f5354,
+	side: THREE.DoubleSide,
+	transparent: true,
+	opacity: 0,
+});
+const plane = new THREE.Mesh(geometry, material);
+plane.rotateX(-Math.PI / 2);
+scene.add(plane);
+
+export const camera = new THREE.PerspectiveCamera(
+	100,
+	myCanvas.offsetWidth / myCanvas.offsetHeight
+);
+
+// create grid helper
+const size = 20;
+const divisions = 20;
+const colorCenterLine = 0x475b74;
+const colorGrid = 0x475b74;
+
+const grid = new THREE.GridHelper(size, divisions, colorCenterLine, colorGrid);
+grid.name = "grid";
+scene.add(grid);
+
+/*
+	Light in 3D scene
+	set(x,y,z)
+		+x front
+		+y up
+		+z left
+*/
+
+const ambientLight = new THREE.HemisphereLight(
+	"white", // bright sky color
+	"grey", // dim ground color
+	1 // intensity
+);
+ambientLight.name = "ambientLight";
+scene.add(ambientLight);
+
+var dirLight = new THREE.DirectionalLight(0x404040, 2);
+dirLight.name = "dirLight";
+dirLight.position.set(100, 100, 100);
+
+dirLight.castShadow = true;
+
+scene.add(dirLight);
+
+// Camera position
+camera.position.set(3, 2, 2);
+camera.lookAt(scene.position);
+
+const renderer = new THREE.WebGLRenderer({ canvas: myCanvas });
+renderer.setClearColor(0xffffff, 1.0);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(myCanvas.offsetWidth, myCanvas.offsetHeight);
+
+export const orbitControls = new OrbitControls(camera, renderer.domElement);
+
+export const loader = new GLTFLoader();
+loader.name = "loader";
+
+let path = "files/" + "SR100C_v1.glb";
+
+loader.load(
+	path,
+	function (gltf) {
+		let file3D = gltf.scene;
+		file3D.name = "file3D";
+		scene.add(file3D);
+	},
+	undefined,
+	function (error) {
+		console.error(error);
+	}
+);
+
+renderer.setAnimationLoop(() => {
+	orbitControls.update();
+
+	renderer.render(scene, camera);
+});
+
+// Resize canvas
+myCanvas.style.width = window.innerWidth + "px";
+myCanvas.style.height = window.innerHeight + "px";
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+
+window.addEventListener("resize", () => {
+	myCanvas.style.width = window.innerWidth + "px";
+	myCanvas.style.height = window.innerHeight + "px";
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+});
